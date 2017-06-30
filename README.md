6d586d4e3fec4cf59642e0f5f87620ab4ca6e65a
Viranch Mehta <email@viranch.me>
2017-06-30 11:39:49 -0700

    Awkward first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Amazon URL Sanitizer
+============
+
+Chrome extension to remove long ass unnecessary tracking params from URLs on Amazon.
+
+How to use
+==========
+
+Download the zipped extension from [here](https://github.com/viranch/amazon-sanitizer/archive/master.zip) and unzip it somewhere.
+
+Go to <a href="chrome://extensions/">extensions page</a> in Chrome, enable "Developer mode", click on "Load unpacked extension..." and browse to the unzipped folder.
+
+Share clean Amazon URLs.
---
+++ sanitize.js
@@ -0,0 +1,62 @@
+/*
+Author: Viranch Mehta
+Email: email@viranch.me
+*/
+
+// 1. Auto-check Secure Access Image confirmation box
+// 2. Auto-select 1st option from annoying dropdowns
+
+// Ideal would be injecting `$('a[tabindex="4"]').click()` but jquery inside chrome extn is a pain :(
+var inject = '(' + function() {
+	function parse_query(query) {
+	  var result = {};
+	  query.split("&").forEach(function(part) {
+		var item = part.split("=");
+		result[item[0]] = decodeURIComponent(item[1]);
+	  });
+	  return result;
+	}
+	var observeDOM = (function(){
+		var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
+			eventListenerSupported = window.addEventListener;
+
+		return function(obj, callback){
+			if( MutationObserver ){
+				// define a new observer
+				var obs = new MutationObserver(function(mutations, observer){
+					if( mutations[0].addedNodes.length || mutations[0].removedNodes.length )
+						callback();
+				});
+				// have the observer observe foo for changes in children
+				obs.observe( obj, { childList:true, subtree:true });
+			}
+			else if( eventListenerSupported ){
+				obj.addEventListener('DOMNodeInserted', callback, false);
+				obj.addEventListener('DOMNodeRemoved', callback, false);
+			}
+		};
+	})();
+    var sanitize = function() {
+        var anchors = document.getElementsByTagName('a');
+        for (var i in anchors) {
+            var a = anchors[i];
+            var old_href = a.href;
+            var href = old_href;
+            if (href == null) continue;
+            if (href.indexOf('&url=') !== -1) {
+                href = a.href = parse_query(href).url || href;
+            }
+            if ((href.indexOf('/dp/') !== -1 || href.indexOf('/gp/product/') !== -1) && href.indexOf('/ref=') !== -1) {
+                href = a.href = href.replace(/ref=.*/, '');
+            }
+            if (old_href != a.href) {
+                console.log(old_href, a.href);
+            }
+        }
+    };
+	observeDOM(document.getElementsByTagName('body')[0], sanitize);
+} + ')();';
+
+var script = document.createElement('script');
+script.textContent = inject;
+(document.head||document.documentElement).appendChild(script);
